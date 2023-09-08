b584fcf151bffa05835166ae511779bad34d216b
daudiqbal951 <daudiqbal951@yahoo.com>
2023-09-09 01:07:09 +0500

    task01 completed

---
+++ task01.js
@@ -0,0 +1,3 @@
+"use strict";
+//Install Node.js, TypeScript and VS Code on your computer
+//Node.js, TypeScript and VS Code has been installed on the computer
---
+++ task01.ts
@@ -0,0 +1,3 @@
+//Install Node.js, TypeScript and VS Code on your computer
+
+//Node.js, TypeScript and VS Code has been installed on the computer
